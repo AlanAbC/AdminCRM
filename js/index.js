861b65cff196b42ea27ae47c5be2023a9f028c9c
alana <alanjiraya77@gmail.com>
2017-10-02 07:46:52 -0500

    muchoo3

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
+//Carga de la pagina
 $(document).ready(function(){
 	$('html').niceScroll();
 	$(document).foundation();
+});
+//Seccion de funcionalidad de menu lateral
+var flagmenusuer = 0;
+$(".menuuserarrow").click(function(event) {
+	if(flagmenusuer == 0){
+		$(".contmenuusuer").slideDown('400');
+		flagmenusuer++;
+	}else{
+		$(".contmenuusuer").slideUp('400');
+		flagmenusuer--;
+	}
+	
 });
